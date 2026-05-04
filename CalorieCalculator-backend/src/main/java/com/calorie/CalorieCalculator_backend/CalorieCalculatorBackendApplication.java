9ed547272c25bb8b55600d75a07e3c954c24689f
Hang Jin <h.jin@ostfalia.de>
2026-05-04 17:06:32 +0200

    feat: init Spring Boot backend project

---
+++ CalorieCalculator-backend/src/main/java/com/calorie/CalorieCalculator_backend/CalorieCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calorie.CalorieCalculator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ CalorieCalculator-backend/src/test/java/com/calorie/CalorieCalculator_backend/CalorieCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calorie.CalorieCalculator_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
